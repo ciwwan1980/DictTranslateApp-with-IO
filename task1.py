3575df595b5e0c7c2b91626b5555f6e8119a6eee
ciwwan1980 <ajilqado@gmail.com>
2023-12-02 19:59:43 +0100

    empty-dic-if-translation-is-in-dict-return it-else-search-in-create-new-instance-of-translare

--- task1.py
+++ task1.py
@@ -1,6 +1,16 @@
+from translate import Translator
+import json
 
+def load_dictionary():
+    try:
+        with open('dictionary.json', 'r') as file:
+            dictionary = json.load(file)
+        return dictionary
+    except FileNotFoundError:
+        return {}
 
 def main():
+    dictionary = {} 
 
     while True: 
         print("1.Translate word")
@@ -10,10 +20,21 @@ def main():
         print("4. Exit")
 
 
-        choice=input("Enter your choices (1/2/3/4/5)")
+        choice=input("Enter your choices (1/2/3/4/5) ")
 
-        if choice == "1":
-            pass
+        if choice == '1':
+
+            word_to_translate = input("Enter the word to translate: ")
+            
+            translation = dictionary.get(word_to_translate, None)
+
+            if translation is None:
+                translator = Translator(to_lang="de")
+                translation = translator.translate(word_to_translate)
+                print(f"Translation: {translation}")
+            else:
+                print(f"Translation: {translation}")
+
         elif choice == '2':
              pass
         elif choice == '3':
