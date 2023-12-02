b06cfb2c52c7578db143bce319d13e82606278d3
ciwwan1980 <ajilqado@gmail.com>
2023-12-02 18:33:56 +0100

    add choices-user

--- task1.py
+++ task1.py
@@ -1,14 +1,30 @@
-from translate import Translator
 
-def translate_to_german(word):
-    translator = Translator(to_lang="de")
-    translation = translator.translate(word)
-    return translation
 
 def main():
-    word_to_translate = input("Enter the word to translate to German: ")
-    translated_word = translate_to_german(word_to_translate)
-    print(f"Translation: {translated_word}")
+
+    while True: 
+        print("1.Translate word")
+        print("2. Add translation")
+        print("3.List of all translation")
+        print("4. remove translation")
+        print("4. Exit")
+
+
+        choice=input("Enter your choices (1/2/3/4/5)")
+
+        if choice == "1":
+            pass
+        elif choice == '2':
+             pass
+        elif choice == '3':
+             pass
+        elif choice == '4':
+             pass
+        elif choice == '5':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, 3, 4, or 5.")
 
 if __name__ == "__main__":
     main()
